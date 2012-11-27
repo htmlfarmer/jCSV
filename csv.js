0c2177cf8b7349df805dd9a8126a47d97e8a4293
V$H. <ashermartin@gmail.com>
2012-11-27 11:45:58 -0600

    Update csv.js
    Slight update to include options for parseInt() parseFlot().  Note that the input params is optional. var params = {object : false, parse : true};  Works great... tested carefully.

--- csv.js
+++ csv.js
@@ -1,27 +1,32 @@
 /*
 
-    CSV Reader - Reads a CSV text string and also converts it into a 2D array of floats/integers/strings
-    Version: 3.0
-    
-    Example: 
-            [[row0],[row1],[row2],...] = CSV(text);
-    
-    Usage:  <script type="text/javascript" src="csv.js"></script>
-            var table = new CSV(text);
-            var element = table[3][2]; // element = row 3 column 2 (string/float/int)
-    
-    CSV Specification: http://en.wikipedia.org/wiki/Comma-separated_values
+CSV Reader - Reads a CSV text string and also converts it into a 2D array of floats/integers/strings
+Version: 3.5
+Example:
+[[row0],[row1],[row2],...] = CSV(text);
+Usage: <script type="text/javascript" src="csv.js"></script>
+var table = new CSV(text);
+var element = table[3][2]; // element = row 3 column 2 (string/float/int)
+CSV Specification: http://en.wikipedia.org/wiki/Comma-separated_values
 
-    TODO:  add support for # comment lines (CSV spec?)
-    TODO:  Add code to convert 10.0% to .1 (this code will convert $ to regular floats)
-      
-    Tested with various Microsoft CSV Files with wierd "A""" and ","""
-    
+TODO: add support for # comment lines (CSV spec?)
+TODO: Add code to convert 10.0% to .1 (this code will convert $ to regular floats)
+Tested with various Microsoft CSV Files with wierd "A""" and ","""
 */
 
-function CSV(text){
+function CSV(text, params){ // type is optional - useful for returning objects or speedup if you dont want to parse the numbers out
 
+  var type = {object : false, // if true: [{row},{row},{row},{}] else [[row],[row],[row]] without object lebels
+              parse : true // if true: parseFloat() and parseInt() are used otherwise not
+              };
+
+  if(params) {
+    type.object = params.object ? params.object != undefined : true;
+    type.parse = params.parse ? params.parse != undefined : true;
+  }
+    
   this.csv = [];
+  var header = true;
   
   var comma = ",;";
   var quote = "\"";
@@ -32,19 +37,24 @@ function CSV(text){
   var quoted = false;
 
   // BUG/TODO: add code to convert %
-  var num = function(str){
-    var value = str.replace(/[$% ,]/g, '');
-    if(isNaN(value) || value == ""){ // is it not a number?
-      return str;
-    }
-    else { // its a number... 
-      if(value.indexOf(".") >= 0) { // TODO/BUG: check for %   
-         return parseFloat(value); // replace floating point
-      } 
-      else {
-         return parseInt(value); // replace integers
+  var PARSE = function(str){
+    if(type.parse){
+      var value = str.replace(/[$% ,]/g, '');
+      if(isNaN(value) || value == ""){ // is it not a number?
+        return str;
+      }
+      else { // its a number...
+        if(value.indexOf(".") >= 0) { // TODO/BUG: check for %
+           return parseFloat(value); // replace floating point
+        }
+        else {
+           return parseInt(value); // replace integers
+        }
       }
     } 
+    else { 
+      return str;
+    }
   };
   
   for (var i = 0; i < text.length; i++){
@@ -52,9 +62,9 @@ function CSV(text){
       if(!quoted) { // first quotation mark?
         quoted = true;
         element = ""; // just to be safe clear the element
-      } 
+      }
       else if (comma.indexOf(text[i+1]) >= 0) { // last quotation mark with a , or ; next?
-        line.push(num(element));
+        line.push(PARSE(element));
         i = i + 1; // dont add a , to the CSV skip it
         element = "";
         quoted = false;
@@ -64,12 +74,12 @@ function CSV(text){
         i = i + 1; // skip one " because in the CSV Standard "" = "
       }
       else if (newline.indexOf(text[i+1]) >= 0){
-        line.push(num(element));
-        element = ""; 
+        line.push(PARSE(element));
+        element = "";
         quoted = false; // reset the quote becuse it was the last quote
       }
       else { // Error Case
-        element += text[i]; 
+        element += text[i];
         quoted = false; // else do nothing ... element = element;
       }
     }
@@ -78,18 +88,33 @@ function CSV(text){
         element += text[i];
       }
       else {
-        line.push(num(element));
-        element = "";    
+        line.push(PARSE(element));
+        element = "";
       }
-    } 
+    }
     else if(newline.indexOf(text[i]) >= 0){
       if(quoted){ // if its inside a "" block always add the element
         element += text[i];
-      } 
+      }
       else {
-        line.push(num(element));
-        element = ""; // clear the element
-        this.csv.push(line);
+        line.push(PARSE(element));
+        element = ""; // clear the element and prep it for the next
+        if(type.object == false) {
+          this.csv.push(line);
+        } 
+        else { // push the object into the CSV
+          if (header) {
+            this.titles = line;
+            header = false;
+          }
+          else { 
+            var obj = {};
+            for(var j = 0; j < line.length; j++) {
+              obj[this.titles[j]] = line[j];
+            }
+            this.csv.push(obj);
+          }
+        }
         line = []; // clear the line and go to the next one
         if(newline.indexOf(text[i+1]) >= 0){
           i = i + 1; // skip one element because there are two carrage returns
